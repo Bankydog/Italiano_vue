0364231404cf2b6bbad5bf5df2151d4b6ed6febf
Bank <blankinfear@gmail.com>
2025-02-27 23:58:09 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import Landing from "./components/pages/Landing.vue";
+</script>
+
+<template><Landing /></template>
+
---
+++ src/components/pages/Landing.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-9xl">HIIIIIII</h1>
+  </div>
+</template>
