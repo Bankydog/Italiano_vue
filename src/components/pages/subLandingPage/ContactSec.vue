84b96eaeccf327ab317f864acd6ef8b7fe99b2eb
Bank <blankinfear@gmail.com>
2025-02-28 01:50:15 +0700

    separate a landing page

---
+++ src/components/pages/subLandingPage/ContactSec.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="">
+    <h1>Welcome to ContactSec</h1>
+    <p>Experience the authentic taste of Italy.</p>
+  </section>
+</template>
+  
---
+++ src/components/pages/subLandingPage/HeroSec.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="">
+    <h1>Welcome to Our Italian Restaurant</h1>
+    <p>Experience the authentic taste of Italy.</p>
+  </section>
+</template>
